e17d1ab4025530c4d2123e1d57302d8754197ddd
Alberto Rota <alberto.rota.jimenez@gmail.com>
2019-06-24 23:03:36 +0200

    Mine table is now done.

--- scripts/ees/transmutationtable/EES_transmutationtableui_upgrade.lua
+++ scripts/ees/transmutationtable/EES_transmutationtableui_upgrade.lua
@@ -7,16 +7,19 @@ EES_superInit = init
 function init()
   if EES_superInit then EES_superInit() end
   self.upgradeMaterials = world.getObjectParameter(pane.containerEntityId(), "eesUpgradeMaterials")
-  if not self.upgradeMaterials then
-    widget.setVisible("btnUpgrade", false)
-  elseif not playerCanUpgrade() then
-    widget.setButtonEnabled("btnUpgrade", false)
-  end
+  self.canUpgrade = playerCanUpgrade()
+
+  initUpgradeTooltip()
 end
 
 --------------------------------------------------------------------------------
 -------------------------- Buttons hooks and functions -------------------------
 --------------------------------------------------------------------------------
+
+-- checkBoxUpgrade
+function checkBoxUpgrade()
+  widget.setVisible("layoutUpgrade", not widget.active("layoutUpgrade"))
+end
 
 -- btnUpgrade
 function btnUpgrade()
@@ -29,6 +32,78 @@ end
 --------------------------------------------------------------------------------
 ------------------------------ Private functions -------------------------------
 --------------------------------------------------------------------------------
+
+function initUpgradeTooltip()
+  if not self.upgradeMaterials then return false end
+  local numItems = #self.upgradeMaterials
+
+  -- Set the correct images for "Upgrade" checkbox.
+  if self.canUpgrade then
+    widget.setButtonImages(
+      "checkBoxUpgrade",
+      {base = "/interface/ees/transmutationtable/EES_upgradeReady.png"}
+    )
+    widget.setButtonCheckedImages(
+      "checkBoxUpgrade",
+      {base = "/interface/ees/transmutationtable/EES_upgradeReadyChecked.png"}
+    )
+  end
+
+  -- Enable or disable the "Upgrade" button.
+  widget.setButtonEnabled("layoutUpgrade.btnUpgrade", self.canUpgrade)
+
+  -- Update the height of the tooltip to match the contents
+  local itemList = config.getParameter("gui.layoutUpgrade.children.upgradeIngredientsList.schema")
+  local memberSize, spacing = itemList.memberSize, itemList.spacing
+  local backgroundSize = widget.getSize("layoutUpgrade.background")
+  local listHeight = numItems * (memberSize[2] + spacing[2]) - spacing[2]
+  widget.setSize(
+    "layoutUpgrade",
+    {backgroundSize[1], backgroundSize[2] + listHeight}
+  )
+  widget.setSize(
+    "layoutUpgrade.background",
+    {backgroundSize[1], backgroundSize[2] + listHeight}
+  )
+
+  -- Update the position of the tooltip to match the contents
+  local layoutPos = widget.getPosition("layoutUpgrade")
+  widget.setPosition(
+    "layoutUpgrade",
+    {layoutPos[1], layoutPos[2] - listHeight}
+  )
+
+  -- Update the position of the title to remain on the top
+  local titlePosition = widget.getPosition("layoutUpgrade.title")
+  widget.setPosition(
+    "layoutUpgrade.title",
+    {titlePosition[1], titlePosition[2] + listHeight}
+  )
+
+  -- Add all items to the list.
+  local ingredientsList = "layoutUpgrade.upgradeIngredientsList"
+  widget.clearListItems(ingredientsList)
+  for _, upgradeMaterial in pairs(self.upgradeMaterials) do
+    local itemName, count = upgradeMaterial.item, upgradeMaterial.count
+    local itemConfig = root.itemConfig(itemName).config
+    local newItem = string.format(
+      "%s.%s",
+      ingredientsList,
+      widget.addListItem(ingredientsList)
+    )
+
+    -- Basic info.
+    widget.setText(newItem..".itemName", itemConfig.shortdescription)
+    widget.setItemSlotItem(
+      newItem..".itemIcon",
+      { name = itemName, count = 1 }
+    )
+    widget.setText(
+      newItem..".count",
+      player.hasCountOfItem(itemName).."/"..count
+    )
+  end
+end
 
 function playerCanUpgrade()
   if not self.upgradeMaterials then return false end
