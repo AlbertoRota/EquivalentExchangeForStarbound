b376715bba95de3ae649c4f8eac9ff669bc6a6f8
Alberto Rota <alberto.rota.jimenez@gmail.com>
2019-07-15 21:48:22 +0200

    Created skeleton for the "Universal" tablet

---
+++ items/EES/universaltransmutationtablet/EES_universaltransmutationtablet.lua
@@ -0,0 +1,24 @@
+function activate(fireMode, shiftHeld)
+  -- Load the basic UI config
+  local configData = root.assetJson("/interface/ees/universaltransmutationtablet/EES_tritablet.config")
+
+  -- Pass the object specific config to the UI
+  configData.eesMainEmc = config.getParameter("eesMainEmc")
+  configData.eesSlotConfig = config.getParameter("eesSlotConfig")
+  configData.eesCanStudy = config.getParameter("eesCanStudy")
+  configData.eesTitle = config.getParameter("shortdescription")
+  configData.eesTitleIcon = config.getParameter("inventoryIcon")
+
+  -- Open the UI
+  activeItem.interact("ScriptPane", configData)
+  animator.playSound("activate")
+end
+
+
+function update()
+  if mcontroller.crouching() then
+		activeItem.setArmAngle(-0.15)
+	else
+		activeItem.setArmAngle(-0.5)
+	end
+end
